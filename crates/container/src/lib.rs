serpent-os/tools

Reduce size of unsafe blocks to the required minimum

crates/container/src/lib.rs
@@ -124,33 +124,27 @@ impl Container {
         }
 
         let sync_raw = (sync_r.as_raw_fd(), sync_w.as_raw_fd());
-        let pid = unsafe {
-            clone(
-                Box::new(|| match enter(&self, sync_raw, &mut f) {
-                    Ok(_) => 0,
-                    // Write error back to parent process
-                    Err(error) => {
-                        let error = format_error(error);
-                        let mut pos = 0;
+        let clone_cb = Box::new(|| match enter(&self, sync_raw, &mut f) {
+            Ok(_) => 0,
+            // Write error back to parent process
+            Err(error) => {
+                let error = format_error(error);
+                let mut pos = 0;
 
-                        while pos < error.len() {
-                            let Ok(len) = write(&sync_w, &error.as_bytes()[pos..]) else {
-                                break;
-                            };
+                while pos < error.len() {
+                    let Ok(len) = write(&sync_w, &error.as_bytes()[pos..]) else {
+                        break;
+                    };
 
-                            pos += len;
-                        }
+                    pos += len;
+                }
 
-                        _ = close(sync_w.as_raw_fd());
+                _ = close(sync_w.as_raw_fd());
 
-                        1
-                    }
-                }),
-                &mut *addr_of_mut!(STACK),
-                flags,
-                Some(SIGCHLD),
-            )?
-        };
+                1
+            }
+        });
+        let pid = unsafe { clone(clone_cb, &mut *addr_of_mut!(STACK), flags, Some(SIGCHLD))? };
 
         // Update uid / gid map to map current user to root in container
         if rootless {

AerynOS/os-tools

crates/container/src/lib.rs
@@ -123,33 +123,27 @@ impl Container {
             flags |= CloneFlags::CLONE_NEWNET;
         }
 
-        let pid = unsafe {
-            clone(
-                Box::new(|| match enter(&self, sync, &mut f) {
-                    Ok(_) => 0,
-                    // Write error back to parent process
-                    Err(error) => {
-                        let error = format_error(error);
-                        let mut pos = 0;
+        let clone_cb = Box::new(|| match enter(&self, sync, &mut f) {
+            Ok(_) => 0,
+            // Write error back to parent process
+            Err(error) => {
+                let error = format_error(error);
+                let mut pos = 0;
 
-                        while pos < error.len() {
-                            let Ok(len) = write(sync.1, &error.as_bytes()[pos..]) else {
-                                break;
-                            };
+                while pos < error.len() {
+                    let Ok(len) = write(sync.1, &error.as_bytes()[pos..]) else {
+                        break;
+                    };
 
-                            pos += len;
-                        }
+                    pos += len;
+                }
 
-                        let _ = close(sync.1);
+                let _ = close(sync.1);
 
-                        1
-                    }
-                }),
-                &mut *addr_of_mut!(STACK),
-                flags,
-                Some(SIGCHLD),
-            )?
-        };
+                1
+            }
+        });
+        let pid = unsafe { clone(clone_cb, &mut *addr_of_mut!(STACK), flags, Some(SIGCHLD))? };
 
         // Update uid / gid map to map current user to root in container
         if rootless {