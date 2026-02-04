31014e74bbae741207fc75284e4fa41af9bf8e68
Cory Forsstrom <cforsstrom18@gmail.com>
2026-02-04 13:58:53 -0800

    moss: Add flag to disable system-model warning

--- moss/src/system_model/decode.rs
+++ moss/src/system_model/decode.rs
@@ -6,6 +6,15 @@ use crate::{Provider, Repository, SystemModel, dependency, repository};
 pub fn decode(content: &str) -> Result<SystemModel, Error> {
     let document: KdlDocument = content.parse().map_err(Error::ParseKdlDocument)?;
 
+    let disable_warning = document
+        .get_arg("disable_warning")
+        .map(|value| {
+            value
+                .as_bool()
+                .ok_or(Error::InvalidRootValue("disable_warning", "bool", value.to_string()))
+        })
+        .transpose()?;
+
     let packages = document
         .get("packages")
         .map(|node| node.iter_children().map(decode_package).collect::<Result<_, _>>())
@@ -19,6 +28,7 @@ pub fn decode(content: &str) -> Result<SystemModel, Error> {
         .unwrap_or_default();
 
     Ok(SystemModel {
+        disable_warning: disable_warning.unwrap_or_default(),
         repositories,
         packages,
         encoded: content.to_owned(),
@@ -35,7 +45,7 @@ fn decode_repository(node: &KdlNode) -> Result<(repository::Id, Repository), Err
 
     let description = get_child_value(node, "description")
         .map(|value| {
-            value.as_string().ok_or(Error::InvalidValue(
+            value.as_string().ok_or(Error::InvalidNodeValue(
                 "repository",
                 name.to_owned(),
                 "description",
@@ -47,7 +57,7 @@ fn decode_repository(node: &KdlNode) -> Result<(repository::Id, Repository), Err
         .unwrap_or_default();
     let uri = get_child_value(node, "uri")
         .map(|value| {
-            value.as_string().ok_or(Error::InvalidValue(
+            value.as_string().ok_or(Error::InvalidNodeValue(
                 "repository",
                 name.to_owned(),
                 "uri",
@@ -61,7 +71,7 @@ fn decode_repository(node: &KdlNode) -> Result<(repository::Id, Repository), Err
         .map_err(|err| Error::ParseRepositoryUri(err, name.to_owned()))?;
     let enabled = get_child_value(node, "enabled")
         .map(|value| {
-            value.as_bool().ok_or(Error::InvalidValue(
+            value.as_bool().ok_or(Error::InvalidNodeValue(
                 "repository",
                 name.to_owned(),
                 "uri",
@@ -73,7 +83,7 @@ fn decode_repository(node: &KdlNode) -> Result<(repository::Id, Repository), Err
         .unwrap_or(true);
     let priority = get_child_value(node, "priority")
         .map(|value| {
-            let int = value.as_integer().ok_or(Error::InvalidValue(
+            let int = value.as_integer().ok_or(Error::InvalidNodeValue(
                 "repository",
                 name.to_owned(),
                 "priority",
@@ -106,8 +116,10 @@ fn get_child_value<'a>(node: &'a KdlNode, name: &str) -> Option<&'a KdlValue> {
 
 #[derive(Debug, Error)]
 pub enum Error {
+    #[error("invalid value for {0}, expected {1} got {2}")]
+    InvalidRootValue(&'static str, &'static str, String),
     #[error("invalid value for {0} {1} {2}, expected {3} got {4}")]
-    InvalidValue(&'static str, String, &'static str, &'static str, String),
+    InvalidNodeValue(&'static str, String, &'static str, &'static str, String),
     #[error("missing {0} for {1} {2}")]
     MissingValue(&'static str, &'static str, String),
     #[error("parse as kdl document")]
