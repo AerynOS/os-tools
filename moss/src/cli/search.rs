236cbf6b96cac4835e9bcd1b6847e8d51e2d952a
Jonathan Lopez <jonathanglopez@gmail.com>
2026-03-04 12:40:59 -0500

    Explicitly label output type

--- moss/src/cli/search.rs
+++ moss/src/cli/search.rs
@@ -56,7 +56,7 @@ pub fn handle(args: &ArgMatches, installation: Installation) -> Result<(), Error
         package::Flags::new().with_available()
     };
 
-    let output = if provides {
+    let output: Vec<Output> = if provides {
         search_providing_packages(client, flags, keyword)
     } else {
         search_packages(client, flags, keyword)
