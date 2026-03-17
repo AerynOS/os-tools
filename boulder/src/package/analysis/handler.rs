serpent-os/tools

moss/boulder: Add perl providers
Reads the "package" from .pm files to grok the provider.

boulder/src/package/analysis/handler.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::{path::PathBuf, process::Command};
 
 use moss::{dependency, Dependency, Provider};
@@ -112,6 +114,40 @@ pub fn pkg_config(bucket: &mut BucketMut, info: &mut PathInfo) -> Result<Respons
             kind,
             name: dep.to_string(),
         });
+    }
+
+    Ok(Decision::NextHandler.into())
+}
+
+pub fn perl(bucket: &mut BucketMut, info: &mut PathInfo) -> Result<Response, BoxError> {
+    let file_path = info.path.clone().into_os_string().into_string().unwrap_or_default();
+    let is_pm_file = file_path.contains("perl") && info.file_name().ends_with(".pm");
+
+    if !is_pm_file {
+        return Ok(Decision::NextHandler.into());
+    }
+
+    let reader = BufReader::new(File::open(&info.path)?);
+
+    for line in reader.lines() {
+        match line {
+            Ok(line) => {
+                if line.starts_with("package") {
+                    let perl_module = line
+                        .strip_prefix("package")
+                        .unwrap()
+                        .trim_start()
+                        .strip_suffix(";")
+                        .unwrap_or_default();
+                    bucket.providers.insert(Provider {
+                        kind: dependency::Kind::Perl,
+                        name: perl_module.to_string(),
+                    });
+                    break;
+                }
+            }
+            Err(e) => println!("ERROR: {}", e),
+        }
     }
 
     Ok(Decision::NextHandler.into())

crates/stone/src/payload/meta.rs
@@ -50,6 +50,9 @@ pub enum Dependency {
 
     /// An emul32-compatible pkgconfig .pc dependency (lib32/*.pc)
     PkgConfig32,
+
+    /// A Perl module
+    Perl,
 }
 
 #[repr(u8)]
@@ -146,6 +149,7 @@ fn decode_dependency(i: u8) -> Result<Dependency, DecodeError> {
         6 => Dependency::Binary,
         7 => Dependency::SystemBinary,
         8 => Dependency::PkgConfig32,
+        9 => Dependency::Perl,
         _ => return Err(DecodeError::UnknownDependency(i)),
     };
     Ok(result)

AerynOS/os-tools

boulder/src/package/analysis/handler.rs
@@ -1,7 +1,7 @@
 use filetime::FileTime;
 use itertools::Itertools;
 use std::{
-    io::{BufReader, BufWriter, Write},
+    io::{BufRead, BufReader, BufWriter, Write},
     os::unix::fs::symlink,
     path::{Component, Path, PathBuf},
     process::Command,
@@ -59,6 +59,41 @@ pub fn binary(bucket: &mut BucketMut<'_>, info: &mut PathInfo) -> Result<Respons
             name: info.file_name().to_owned(),
         };
         bucket.providers.insert(provider);
+    }
+
+    Ok(Decision::NextHandler.into())
+}
+
+// Parse the 'package' value from a .pm file e.g. package Foo::Bar;
+pub fn perl(bucket: &mut BucketMut<'_>, info: &mut PathInfo) -> Result<Response, BoxError> {
+    let file_path = info.path.clone().into_os_string().into_string().unwrap_or_default();
+    let is_pm_file = file_path.contains("perl") && info.file_name().ends_with(".pm");
+
+    if !is_pm_file {
+        return Ok(Decision::NextHandler.into());
+    }
+
+    let reader = BufReader::new(File::open(&info.path)?);
+
+    for line in reader.lines() {
+        match line {
+            Ok(line) => {
+                if line.starts_with("package") {
+                    let perl_module = line
+                        .strip_prefix("package")
+                        .unwrap()
+                        .trim_start()
+                        .strip_suffix(";")
+                        .unwrap_or_default();
+                    bucket.providers.insert(Provider {
+                        kind: dependency::Kind::Perl,
+                        name: perl_module.to_owned(),
+                    });
+                    break;
+                }
+            }
+            Err(e) => println!("ERROR: {e}"),
+        }
     }
 
     Ok(Decision::NextHandler.into())

crates/stone/src/payload/meta.rs
@@ -51,6 +51,9 @@ pub enum StonePayloadMetaDependency {
     /// An emul32-compatible pkgconfig .pc dependency (lib32*.pc)
     PkgConfig32 = 8,
 
+    /// A Perl module
+    Perl = 9,
+
     Unknown = 255,
 }
 
@@ -152,6 +155,7 @@ fn decode_dependency(i: u8) -> StonePayloadMetaDependency {
         6 => StonePayloadMetaDependency::Binary,
         7 => StonePayloadMetaDependency::SystemBinary,
         8 => StonePayloadMetaDependency::PkgConfig32,
+        9 => StonePayloadMetaDependency::Perl,
         _ => StonePayloadMetaDependency::Unknown,
     }
 }