91f9c6a0e99810a96634f339957ad97f9214e0bb
Cory Forsstrom <cforsstrom18@gmail.com>
2026-04-17 08:34:28 -0700

    moss: Enable adding / listing root index repos

--- moss/src/cli/repo.rs
+++ moss/src/cli/repo.rs
@@ -1,9 +1,9 @@
 // SPDX-FileCopyrightText: 2023 AerynOS Developers
 // SPDX-License-Identifier: MPL-2.0
 
-use std::{path::PathBuf, process};
+use std::{collections::BTreeMap, path::PathBuf, process, str::FromStr};
 
-use clap::{Arg, ArgAction, ArgMatches, Command, arg};
+use clap::{Arg, ArgAction, ArgMatches, Command, arg, builder::ValueParser};
 use itertools::Itertools;
 use moss::{
     Installation, Repository, environment,
@@ -18,8 +18,8 @@ use url::Url;
 enum Action {
     // Root
     List,
-    // Root, Id, Url, Comment
-    Add(String, Url, String, Priority),
+    // Root, Id, Url, Comment, Root index enabled options
+    Add(String, Url, String, Priority, Option<RootIndexOptions>),
     // Root, Id
     Remove(String),
     // Root, Id
@@ -54,6 +54,25 @@ pub fn command() -> Command {
                         .action(ArgAction::Set)
                         .default_value("0")
                         .value_parser(clap::value_parser!(u64)),
+                )
+                .next_help_heading("Root index")
+                // TODO: Completely overhaul this CLI API, this is temporary to add support
+                // initially for adding the new root index repo source without breaking
+                // the current API
+                .arg(
+                    Arg::new("root-index")
+                        .long("root-index")
+                        .value_name("root-index-options")
+                        .default_missing_value("version=stream/unstable")
+                        .help(concat!(
+                            "Defines the repo via root index options where <URI> is the base-uri ",
+                            "and all other options are passed to this flag\n\n",
+                            "Example: --root-index [default: version=stream/unstable]\n",
+                            "Example: --root-index=version=stream/volatile",
+                        ))
+                        .action(ArgAction::Set)
+                        .num_args(0..=1)
+                        .value_parser(ValueParser::new(parse_root_index_options)),
                 ),
         )
         .subcommand(
@@ -119,6 +138,7 @@ pub fn handle(args: &ArgMatches, installation: Installation) -> Result<(), Error
             cmd_args.get_one::<Url>("URI").cloned().unwrap(),
             cmd_args.get_one::<String>("comment").cloned().unwrap(),
             Priority::new(*cmd_args.get_one::<u64>("priority").unwrap()),
+            cmd_args.get_one::<RootIndexOptions>("root-index").cloned(),
         ),
         Some(("remove", cmd_args)) => Action::Remove(cmd_args.get_one::<String>("NAME").cloned().unwrap()),
         Some(("enable", cmd_args)) => Action::Enable(cmd_args.get_one::<String>("NAME").cloned().unwrap()),
@@ -129,7 +149,9 @@ pub fn handle(args: &ArgMatches, installation: Installation) -> Result<(), Error
     // dispatch to runtime handler function
     match handler {
         Action::List => list(manager),
-        Action::Add(name, uri, comment, priority) => add(manager, name, uri, comment, priority),
+        Action::Add(name, uri, comment, priority, root_index_options) => {
+            add(manager, name, uri, comment, priority, root_index_options)
+        }
         Action::Remove(name) => remove(manager, name),
         Action::Update(name) => update(manager, name),
         Action::Enable(name) => enable(manager, name),
@@ -144,14 +166,26 @@ fn add(
     uri: Url,
     comment: String,
     priority: Priority,
+    root_index_options: Option<RootIndexOptions>,
 ) -> Result<(), Error> {
     let id = repository::Id::new(&name);
+
+    let source = if let Some(RootIndexOptions { channel, version, arch }) = root_index_options {
+        repository::Source::RootIndex(repository::RootIndexSource {
+            base_uri: uri,
+            channel,
+            version,
+            arch,
+        })
+    } else {
+        repository::Source::DirectIndex(uri)
+    };
 
     manager.add_repository(
         id.clone(),
         Repository {
             description: comment,
-            source: repository::Source::DirectIndex(uri),
+            source,
             priority,
             active: true,
         },
@@ -179,11 +213,21 @@ fn list(manager: repository::Manager) -> Result<(), Error> {
             String::new()
         };
 
-        println!(
-            " - {id} = {} [{}]{disabled}",
-            repo.source.direct_index().expect("TODO UPDATE"),
-            repo.priority
-        );
+        // TODO: Refactor this in future unit of work to print KDL encoded
+        // documents for each repo. The below addition of `RootIndexSource`
+        // is a temporary fix, not the desired future state
+        match &repo.source {
+            repository::Source::DirectIndex(uri) => println!(" - {id} = {uri} [{}]{disabled}", repo.priority),
+            repository::Source::RootIndex(repository::RootIndexSource {
+                base_uri,
+                channel,
+                version,
+                arch,
+            }) => println!(
+                " - {id} = (base-uri={base_uri}, channel={channel}, version={version}, arch={arch}) [{}]{disabled}",
+                repo.priority
+            ),
+        }
     }
 
     Ok(())
@@ -242,6 +286,40 @@ fn disable(mut manager: repository::Manager, repo: String) -> Result<(), Error> 
     println!("{id} disabled");
 
     Ok(())
+}
+
+#[derive(Debug, Clone)]
+struct RootIndexOptions {
+    channel: repository::format::Identifier,
+    version: repository::format::ScopedIdentifier,
+    arch: String,
+}
+
+fn parse_root_index_options(s: &str) -> Result<RootIndexOptions, String> {
+    if !s.contains('=') {
+        return Err("options must be key=value[,key=value]*".to_owned());
+    }
+
+    let mut key_values = s
+        .split(',')
+        .filter_map(|kv| kv.split_once('='))
+        .collect::<BTreeMap<_, _>>();
+
+    let channel =
+        repository::format::Identifier::try_from(key_values.remove("channel").unwrap_or(repository::DEFAULT_CHANNEL))
+            .map_err(|err| err.to_string())?;
+
+    let version =
+        repository::format::ScopedIdentifier::from_str(key_values.remove("version").unwrap_or("stream/unstable"))
+            .map_err(|err| format!("invalid version identifier: {err}"))?;
+
+    let arch = key_values.remove("arch").unwrap_or(repository::DEFAULT_ARCH).to_owned();
+
+    if let Some(key) = key_values.into_keys().next() {
+        return Err(format!("unknown key: {key}"));
+    }
+
+    Ok(RootIndexOptions { channel, version, arch })
 }
 
 #[derive(Debug, Error)]
