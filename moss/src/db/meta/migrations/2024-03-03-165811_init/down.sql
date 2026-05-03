27889c8761642e71001740fe1c78b81972c96232
Fabio Forni <development@redaril.me>
2026-05-04 00:06:15 +0200

    moss: db: Migrate from Diesel to rusqlite

--- moss/src/db/meta/migrations/2024-03-03-165811_init/down.sql
+++
@@ -1,9 +0,0 @@
--- SPDX-FileCopyrightText: 2024 AerynOS Developers
--- SPDX-License-Identifier: MPL-2.0
-
--- This file should undo anything in `up.sql`
-DROP TABLE IF EXISTS meta;
-DROP TABLE IF EXISTS meta_licenses;
-DROP TABLE IF EXISTS meta_dependencies;
-DROP TABLE IF EXISTS meta_providers;
-DROP TABLE IF EXISTS meta_conflicts;
