e82c6a7ae65cdfc3e70e7add2f29bcaca5f95478
Fabio Forni <development@redaril.me>
2026-07-31 23:11:41 +0200

    boulder: draft: monitoring: basic: Accept SourceUri

--- boulder/src/draft/metadata/basic.rs
+++ boulder/src/draft/metadata/basic.rs
@@ -3,12 +3,16 @@
 
 use moss::util;
 use regex::Regex;
-use url::Url;
+use stone_recipe::upstream::SourceUri;
 
 use super::Source;
 
-pub fn source(upstream: &Url) -> Option<Source> {
-    let filename = util::uri_file_name(upstream);
+pub fn source(upstream: &SourceUri) -> Option<Source> {
+    if !matches!(upstream.kind, stone_recipe::upstream::Kind::Archive) {
+        return None;
+    }
+
+    let filename = util::uri_file_name(&upstream.url);
 
     let regex = Regex::new(r"^([a-zA-Z0-9_-]+)-([a-zA-Z0-9._-]+)\.(zip|tar|sh|bin\.*)").unwrap();
     let captures = regex.captures(filename)?;
@@ -16,7 +20,7 @@ pub fn source(upstream: &Url) -> Option<Source> {
     let name = captures.get(1)?.as_str().to_owned();
     let version = captures.get(2)?.as_str().to_owned();
 
-    let (homepage, _) = upstream.as_str().rsplit_once('/')?;
+    let (homepage, _) = upstream.url.as_str().rsplit_once('/')?;
 
     Some(Source {
         name,
