c5f7e67581e825e6f1e201ca427d6e8ea962d5ac
Fabio Forni <development@redaril.me>
2026-07-31 23:43:52 +0200

    boulder: draft: monitoring: pypi: Accept SourceUri

--- boulder/src/draft/metadata/pypi.rs
+++ boulder/src/draft/metadata/pypi.rs
@@ -3,19 +3,23 @@
 
 use moss::util;
 use regex::Regex;
-use url::Url;
+use stone_recipe::upstream::SourceUri;
 
 use super::Source;
 
-pub fn source(upstream: &Url) -> Option<Source> {
+pub fn source(upstream: &SourceUri) -> Option<Source> {
+    if !matches!(upstream.kind, stone_recipe::upstream::Kind::Archive) {
+        return None;
+    }
+
     let regex = Regex::new(
         r"^https://files\.pythonhosted\.org/packages/[a-f0-9]{2}/[a-f0-9]{2}/[a-f0-9]+/([^/]+)-([\d.]+)\.tar\.gz$",
     )
     .unwrap();
 
-    let filename = util::uri_file_name(upstream);
+    let filename = util::uri_file_name(&upstream.url);
 
-    let captures = regex.captures(upstream.as_str())?;
+    let captures = regex.captures(upstream.url.as_str())?;
 
     let name = captures.get(1)?.as_str().to_owned();
     let version = captures.get(2)?.as_str().to_owned();
@@ -44,9 +48,12 @@ mod tests {
     #[test]
     fn test_regex_typical_pypi_url() {
         let url_str = "https://files.pythonhosted.org/packages/59/83/a60af4e83c492c7dceceeabd677aa87bbaf2d8910b3d1b973295e560f421/pyzk-0.9.tar.gz";
-        let url = Url::parse(url_str).unwrap();
+        let uri = SourceUri {
+            kind: stone_recipe::upstream::Kind::Archive,
+            url: Url::parse(url_str).unwrap(),
+        };
 
-        let source = source(&url);
+        let source = source(&uri);
         assert!(source.is_some());
 
         let source = source.unwrap();
@@ -57,5 +64,17 @@ mod tests {
             source.uri,
             "https://files.pythonhosted.org/packages/source/p/pyzk/pyzk-0.9.tar.gz"
         );
+    }
+
+    #[test]
+    fn test_git_repo_not_supported() {
+        let url_str = "https://files.pythonhosted.org/packages/59/83/a60af4e83c492c7dceceeabd677aa87bbaf2d8910b3d1b973295e560f421/pyzk-0.9.tar.gz";
+        let uri = SourceUri {
+            kind: stone_recipe::upstream::Kind::Git,
+            url: Url::parse(url_str).unwrap(),
+        };
+
+        let source = source(&uri);
+        assert!(source.is_none());
     }
 }
